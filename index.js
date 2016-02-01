46b2ee0bc3949a04db52c453ec6f524ca355e20c
RM <rm@RMs-MacBook-Pro.fios-router.home>
2016-02-01 15:44:05 -0500

    created menu message function

--- index.js
+++ index.js
@@ -18,7 +18,35 @@ connection.connect(function(err) {
 		welcome: "Welcome to the Zoo And Friends App~!",
 			function(welcome){
 				console.log(this.welcome);
-				return;
+				return (this.welcome);
+			}
+		// The menu message function
+		/*
+		Create a key named menu inside of the zoo object
+
+console logs Enter (A): ------> to Add a new animal to the Zoo!
+console logs Enter (U): ------> to Update info on an animal in the Zoo!
+console logs Enter (V): ------> to Visit the animals in the Zoo!
+console logs Enter (D): ------> to Adopt an animal from the Zoo!
+console logs Enter (Q): ------> to Quit and exit the Zoo!
+
+Hint: To make it pretty when displaying make sure to console log a empty line after every console log with a message
+Hint: this will change many times in your app, because we are using the npm package prompt, 
+and now we're using prompts within prompts, 
+so, you should be storing this into a variable before you use this 
+inside of a prompt callback function. 
+Then you should be using that variable in the next function. 
+This will get clearer as we move on.
+
+*/
+
+	var add = function(input_scope) {
+		var currentScope = input_scope;
+		console.log('To add an animal to the zoo please fill out the following form for us!');
+		prompt.get(name, type, age) {
+			connection.query("")
+		}
 	}
-}
+
+	}
     });
